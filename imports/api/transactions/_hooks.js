418bdc4734c1a65e67b31f57a6a39a91f23cf441
fabienoger <fab-oger@live.fr>
2016-11-14 11:45:22 +0100

    Improve Transaction update hook to update Account balance

--- imports/api/transactions/_hooks.js
+++ imports/api/transactions/_hooks.js
@@ -8,12 +8,17 @@ Transactions.before.insert((userId, doc) => {
   doc.active = true;
 });
 
-Transactions.after.update((userId, doc, fieldNames, modifier) => {
+Transactions.before.update((userId, doc, fieldNames, modifier) => {
   // Check if active is set to false
-  if (_.contains(fieldNames, "active") && modifier.$set.active == false) {
+  if ((_.contains(fieldNames, "active") && modifier.$set.active == false) || _.contains(fieldNames, "value")) {
+    let value = doc.value;
+    if (_.contains(fieldNames, "value")) {
+      value = doc.value - modifier.$set.value
+    }
+
     // Update Account balance
     const accountDoc = {
-      $inc: {balance: parseFloat(doc.value)}
+      $inc: {balance: parseFloat(value)}
     };
     Meteor.call("updateAccount", doc.accountId, accountDoc, function(err, result) {
       if (err) {
