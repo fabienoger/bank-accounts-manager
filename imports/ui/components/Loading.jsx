dead3decb71d71b69c5702ed0ef3cdad3407c291
fabienoger <fab-oger@live.fr>
2016-11-22 13:16:21 +0100

    Add loader

--- imports/ui/components/Loading.jsx
+++ imports/ui/components/Loading.jsx
@@ -4,10 +4,13 @@ import { Meteor }   from 'meteor/meteor';
 
 export default class Loading extends React.Component {
   render() {
+    const loadingStyle = {
+      width: '30px',
+      height: '30px',
+      borderColor: '#457D97'
+    };
     return (
-      <div>
-        Loading ...
-      </div>
+      <div className="loading" style={loadingStyle}></div>
     )
   }
 }
